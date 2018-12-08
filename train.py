ae13fc82afc6375721374fd8992877e6a769f378
zsypotter <zsypotter@gmail.com>
2018-12-08 14:15:58 +0800

    add comment to train.py

--- train.py
+++ train.py
@@ -10,8 +10,8 @@ import argparse
 
 from utils import *
 
+# h-parameter
 parser = argparse.ArgumentParser()
-
 parser.add_argument("--img_folder", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/images")
 parser.add_argument("--att_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/attributes/class_attribute_labels_continuous.txt")
 parser.add_argument("--train_img_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/train.txt")
@@ -28,15 +28,16 @@ parser.add_argument("--batch_size", type=int, default=64)
 parser.add_argument("--weight_decay", type=float, default=0.01)
 parser.add_argument("--m_lr", type=float, default=0.001)
 parser.add_argument("--b_lr", type=float, default=0.00001)
-
 args = parser.parse_args()
 
+# img-preprocess
 data_transforms = transforms.Compose([
             transforms.CenterCrop(args.img_size),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
 
+# load data
 trainset = data_loader.customData(args.img_folder, args.train_img_path, args.train_cls_path, args.train_class_num, data_transforms)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 testRset = data_loader.customData(args.img_folder, args.testR_img_path, args.train_cls_path, args.train_class_num, data_transforms)
@@ -44,12 +45,7 @@ testRloader = torch.utils.data.DataLoader(testRset, batch_size=1, shuffle=True, 
 testZset = data_loader.customData(args.img_folder, args.testZ_img_path, args.test_cls_path, args.test_class_num, data_transforms)
 testZloader = torch.utils.data.DataLoader(testZset, batch_size=1, shuffle=True, num_workers=2)
 
-bk_net = model.Backbone()
-mp_net = model.Mapping(args.att_size)
-
-set_parameter_requires_grad(bk_net, args.finetune)
-set_parameter_requires_grad(mp_net, True)
-
+# att-preprocess
 train_cls_file = open(args.train_cls_path)
 test_cls_file = open(args.test_cls_path)
 lines = train_cls_file.readlines()
@@ -68,11 +64,17 @@ for i in range(args.att_size):
 
 train_att_dict = att_dict[train_cls_dict, :]
 test_att_dict = att_dict[test_cls_dict, :]
-train_att_dict = torch.Tensor(train_att_dict).cuda()
-test_att_dict = torch.Tensor(test_att_dict).cuda()
 
+# load model
+bk_net = model.Backbone()
+mp_net = model.Mapping(args.att_size)
+set_parameter_requires_grad(bk_net, args.finetune)
+set_parameter_requires_grad(mp_net, True)
+
+# define loss
 criterion = nn.CrossEntropyLoss()
 
+# define opt
 if args.finetune:
     optimizer = optim.Adam([
                     {'params': bk_net.parameters(), 'lr': args.b_lr},
@@ -83,10 +85,16 @@ else:
                     {'params': mp_net.parameters(), 'weight_decay': args.weight_decay},
                     ], lr=args.m_lr, betas=(0.9, 0.999), eps=1e-08, amsgrad=False)
 
+# move tensor to cuda
+train_att_dict = torch.Tensor(train_att_dict).cuda()
+test_att_dict = torch.Tensor(test_att_dict).cuda()
 bk_net = bk_net.cuda()
 mp_net = mp_net.cuda()
 
+# main step
 for epoch in range(1000):
+
+      # train step
       train_ac = 0
       if args.finetune:
         bk_net.train()
@@ -115,7 +123,8 @@ for epoch in range(1000):
           predict = torch.argmax(similarity, 1)
           train_ac += (predict == label).sum().item()
       train_ac = train_ac / len(trainloader.dataset)
-
+       
+      # testR step
       testR_ac = 0
       bk_net.eval()
       mp_net.eval()
@@ -130,7 +139,8 @@ for epoch in range(1000):
           predict = torch.argmax(similarity, 1)
           testR_ac += (predict == label).sum().item()
       testR_ac = testR_ac / len(testRloader.dataset)
-
+     
+      # testZ step
       testZ_ac = 0
       for i, data in enumerate(testZloader, 0):
           img, label = data
@@ -144,5 +154,6 @@ for epoch in range(1000):
           predict = torch.argmax(similarity, 1)
           testZ_ac += (predict == label).sum().item()
       testZ_ac = testZ_ac / len(testZloader.dataset)
-
+      
+      # print accurancy
       print(epoch, train_ac, testR_ac, testZ_ac)
