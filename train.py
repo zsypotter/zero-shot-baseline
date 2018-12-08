19e779f45b69c9c1b6be0e46a81d75132276984d
zsypotter <zsypotter@gmail.com>
2018-12-08 14:03:07 +0800

    add args into train.py

--- train.py
+++ train.py
@@ -6,60 +6,65 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.nn.parameter import Parameter
+import argparse
 
 from utils import *
 
-img_folder = "/home/disk3/zhousiyu/dataset/CUB_200_2011/images"
-att_path = "/home/disk3/zhousiyu/dataset/CUB_200_2011/attributes/class_attribute_labels_continuous.txt"
-train_img_path = "/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/train.txt"
-train_cls_path = "/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/train_classes.txt"
-testR_img_path = "/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/testRecg.txt"
-testZ_img_path = "/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/testZS.txt"
-test_cls_path = "/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/test_classes.txt"
-finetune = False
-mse = False
+parser = argparse.ArgumentParser()
 
-train_class_num = 150
-test_class_num = 50
-img_size = 224
-att_size = 312
-batch_size = 64
-weight_decay = 0.01
+parser.add_argument("--img_folder", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/images")
+parser.add_argument("--att_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/attributes/class_attribute_labels_continuous.txt")
+parser.add_argument("--train_img_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/train.txt")
+parser.add_argument("--train_cls_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/train_classes.txt")
+parser.add_argument("--testR_img_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/testRecg.txt")
+parser.add_argument("--testZ_img_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/testZS.txt")
+parser.add_argument("--test_cls_path", type=str, default="/home/disk3/zhousiyu/dataset/CUB_200_2011/zeroshot/test_classes.txt")
+parser.add_argument("--finetune", type=bool, default=False)
+parser.add_argument("--mse", type=bool, default=False)
+parser.add_argument("--train_class_num", type=int, default=150)
+parser.add_argument("--test_class_num", type=int, default=50)
+parser.add_argument("--img_size", type=int, default=224)
+parser.add_argument("--att_size", type=int, default=312)
+parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--weight_decay", type=float, default=0.01)
+parser.add_argument("--m_lr", type=float, default=0.001)
+parser.add_argument("--b_lr", type=float, default=0.00001)
+
+args = parser.parse_args()
 
 data_transforms = transforms.Compose([
-            transforms.CenterCrop(img_size),
+            transforms.CenterCrop(args.img_size),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
 
-trainset = data_loader.customData(img_folder, train_img_path, train_cls_path, train_class_num, data_transforms)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-testRset = data_loader.customData(img_folder, testR_img_path, train_cls_path, train_class_num, data_transforms)
+trainset = data_loader.customData(args.img_folder, args.train_img_path, args.train_cls_path, args.train_class_num, data_transforms)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+testRset = data_loader.customData(args.img_folder, args.testR_img_path, args.train_cls_path, args.train_class_num, data_transforms)
 testRloader = torch.utils.data.DataLoader(testRset, batch_size=1, shuffle=True, num_workers=2)
-testZset = data_loader.customData(img_folder, testZ_img_path, test_cls_path, test_class_num, data_transforms)
+testZset = data_loader.customData(args.img_folder, args.testZ_img_path, args.test_cls_path, args.test_class_num, data_transforms)
 testZloader = torch.utils.data.DataLoader(testZset, batch_size=1, shuffle=True, num_workers=2)
 
 bk_net = model.Backbone()
-mp_net = model.Mapping(att_size)
+mp_net = model.Mapping(args.att_size)
 
-set_parameter_requires_grad(bk_net, finetune)
+set_parameter_requires_grad(bk_net, args.finetune)
 set_parameter_requires_grad(mp_net, True)
 
-train_cls_file = open(train_cls_path)
-test_cls_file = open(test_cls_path)
+train_cls_file = open(args.train_cls_path)
+test_cls_file = open(args.test_cls_path)
 lines = train_cls_file.readlines()
 train_cls_dict = [(int(line.split(' ')[0]) - 1) for line in lines]
 lines = test_cls_file.readlines()
 test_cls_dict = [(int(line.split(' ')[0]) - 1) for line in lines]
 
-att_dict = np.loadtxt(att_path)
+att_dict = np.loadtxt(args.att_path)
 if att_dict.max() > 1.:
     att_dict /= 100.
 att_mean = att_dict[train_cls_dict, :].mean(axis=0)
-for i in range(att_size):
+for i in range(args.att_size):
     att_dict[att_dict[:, i] < 0, i] = att_mean[i]
-for i in range(att_size):
+for i in range(args.att_size):
     att_dict[:, i] = att_dict[:, i] - att_mean[i] + 0.5
 
 train_att_dict = att_dict[train_cls_dict, :]
@@ -67,27 +72,27 @@ test_att_dict = att_dict[test_cls_dict, :]
 train_att_dict = torch.Tensor(train_att_dict).cuda()
 test_att_dict = torch.Tensor(test_att_dict).cuda()
 
-if mse:
+if args.mse:
     criterion = nn.MSELoss()
 else:
     criterion = nn.CrossEntropyLoss()
 
-if finetune:
+if args.finetune:
     optimizer = optim.Adam([
-                    {'params': bk_net.parameters(), 'lr': 0.00001},
-                    {'params': mp_net.parameters(), 'weight_decay': weight_decay},
-                    ], lr=0.0001, betas=(0.9, 0.999), eps=1e-08, amsgrad=False)
+                    {'params': bk_net.parameters(), 'lr': args.b_lr},
+                    {'params': mp_net.parameters(), 'weight_decay': args.weight_decay},
+                    ], lr=args.m_lr, betas=(0.9, 0.999), eps=1e-08, amsgrad=False)
 else:
     optimizer = optim.Adam([
-                    {'params': mp_net.parameters(), 'weight_decay': weight_decay},
-                    ], lr=0.0001, betas=(0.9, 0.999), eps=1e-08, amsgrad=False)
+                    {'params': mp_net.parameters(), 'weight_decay': args.weight_decay},
+                    ], lr=args.m_lr, betas=(0.9, 0.999), eps=1e-08, amsgrad=False)
 
 bk_net = bk_net.cuda()
 mp_net = mp_net.cuda()
 
 for epoch in range(1000):
       train_ac = 0
-      if finetune:
+      if args.finetune:
         bk_net.train()
       else:
         bk_net.eval()
@@ -105,11 +110,8 @@ for epoch in range(1000):
           feature = bk_net(img)
           fake_att = mp_net(feature)
 
-          '''similarity_up = torch.mm(fake_att, train_att_dict.t())
-          similarity_down = torch.mm(torch.norm(fake_att, dim=1, keepdim=True), torch.norm(train_att_dict, dim=1, keepdim=True).t())
-          similarity = similarity_up / similarity_down'''
           similarity = torch.mm(fake_att, train_att_dict.t())
-          if mse:
+          if args.mse:
               loss = criterion(fake_att, att)
           else:
               loss = criterion(similarity, label)
@@ -133,10 +135,6 @@ for epoch in range(1000):
 
           feature = bk_net(img)
           fake_att = mp_net(feature)
-
-          '''similarity_up = torch.mm(fake_att, train_att_dict.t())
-          similarity_down = torch.mm(torch.norm(fake_att, dim=1, keepdim=True), torch.norm(train_att_dict, dim=1, keepdim=True).t())
-          similarity = similarity_up / similarity_down'''
           similarity = torch.mm(fake_att, train_att_dict.t())
           predict = torch.argmax(similarity, 1)
           testR_ac += (predict == label).sum().item()
@@ -151,9 +149,6 @@ for epoch in range(1000):
           feature = bk_net(img)
           fake_att = mp_net(feature)
  
-          '''similarity_up = torch.mm(fake_att, test_att_dict.t())
-          similarity_down = torch.mm(torch.norm(fake_att, dim=1, keepdim=True), torch.norm(test_att_dict, dim=1, keepdim=True).t())
-          similarity = similarity_up / similarity_down'''
           similarity = torch.mm(fake_att, test_att_dict.t())
           predict = torch.argmax(similarity, 1)
           testZ_ac += (predict == label).sum().item()
